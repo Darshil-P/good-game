94d4bc217d639c56df5280d5d805e8ca5a0121c9
Darshil Pipalia <darshilpipalia10@gmail.com>
2023-02-07 10:54:08 +0530

    Add Search Service

---
+++ lib/models/game_model.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+class Game {
+  int id;
+  String name;
+  int? cover;
+  String summary;
+  double? totalRating;
+  int? totalRatingCount;
+  int releaseDate;
+
+  Game({
+    required this.id,
+    required this.name,
+    required this.cover,
+    required this.summary,
+    required this.totalRating,
+    required this.totalRatingCount,
+    required this.releaseDate,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'cover': cover,
+      'summary': summary,
+      'total_rating': totalRating,
+      'total_rating_count': totalRatingCount,
+      'first_release_date': releaseDate,
+    };
+  }
+
+  factory Game.fromMap(Map<String, dynamic> map) {
+    return Game(
+      id: map['id'],
+      name: map['name'],
+      cover: map['cover'],
+      summary: map['summary'] ?? '',
+      totalRating: map['total_rating'] ?? 0.0,
+      totalRatingCount: map['total_rating_count'] ?? 0,
+      releaseDate: map['first_release_date'] ?? -1,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Game.fromJson(String source) => Game.fromMap(json.decode(source));
+}
---
+++ lib/services/search_service.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/game_model.dart';
+import 'api_credentials.dart';
+
+Future<List> search(String query) async {
+  http.Response response = await http.post(
+    Uri.parse('https://api.igdb.com/v4/games'),
+    headers: <String, String>{
+      'Client-ID': IGDB_ClientID,
+      'Authorization': 'Bearer $IGDB_AccessToken',
+      'Accept': 'application/json',
+    },
+    body:
+        'fields name, cover, summary, total_rating, total_rating_count, first_release_date; search "$query"; where category = 0;',
+  );
+
+  late List data;
+
+  if (response.statusCode == 200) {
+    data = jsonDecode(response.body) as List;
+  } else if (response.statusCode == 401) {
+    getAccessToken();
+  }
+
+  for (int i = 0; i < data.length; i++) {
+    data[i] = Game.fromMap(data[i]);
+  }
+  return data;
+}
+
+Future<void> getAccessToken() async {
+  var data = await http.post(
+    Uri.parse(
+        'https://id.twitch.tv/oauth2/token?client_id=$IGDB_ClientID&client_secret=$IGDB_ClientSecret&grant_type=client_credentials'),
+  );
+  print(
+      "Update your access token in services/api_credentials.dart\nAccess Token: \"${jsonDecode(data.body)["access_token"]}\"");
+}
